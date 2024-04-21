42a1857b88fcca8ece863ce3c042099b705c1124
Shohanur Rahaman <shohan4556@gmail.com>
2024-04-21 17:25:52 +0600

    load assets and update background

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -3,6 +3,7 @@ import { Scene, Cameras, Display, GameObjects, Scale } from 'phaser';
 export class Game extends Scene {
     camera: Cameras.Scene2D.Camera;
     background: GameObjects.Image;
+    worldZone: GameObjects.Zone;
     gamewidth: number;
     gameHeight: number;
 
@@ -17,11 +18,13 @@ export class Game extends Scene {
 
     create() {
         this.camera = this.cameras.main;
+        this.worldZone = this.add.zone(this.camera.centerX, this.camera.centerY, this.gamewidth, this.gameHeight);
         this.createBackground();
     }
 
     private createBackground() {
-        this.background = this.add.image(this.camera.centerX, this.camera.centerY, 'background').setOrigin(0.5).setDepth(0);
-        Display.Align.In.Center(this.background, this.add.zone(this.camera.centerX, this.camera.centerY, this.gamewidth, this.gameHeight));
+        this.background = this.add.image(this.camera.centerX, this.camera.centerY, `background-${Phaser.Math.Between(1, 2)}`).setOrigin(0.5).setDepth(0);
+        this.background.setDisplaySize(this.camera.width, this.camera.height);
+        Display.Align.In.Center(this.background, this.worldZone);
     }
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -49,6 +49,17 @@ export class Preloader extends Scene {
         //  Load the assets for the game - Replace with your own assets
         this.load.setPath('assets');
 
+        this.load.image('background-1', 'plinko/bg-star.jpg');
+        this.load.image('background-2', 'plinko/bg-town.jpg');
+        this.load.image('lower-part-1', 'plinko/lower-part-1.png');
+        this.load.image('lower-part-2', 'plinko/lower-part-2.png');
+        this.load.image('coin', 'plinko/coin.png');
+        this.load.image('coin-bg', 'plinko/coin-bg.png');
+        this.load.image('arrow-down', 'plinko/arrow-down.png');
+
+        // load physics shape
+        this.load.json('physicsShape', 'plinko/bucket_shape.json');
+
         this.load.image('background', 'bg.png');
     }
 
