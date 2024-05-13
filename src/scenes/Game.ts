55c08eba1f3580c17c221745c6afc452ae7574aa
Shohanur Rahaman <shohan4556@gmail.com>
2024-05-14 00:23:46 +0600

    Update Game.ts

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,4 +1,4 @@
-import { Scene, Cameras, Display, GameObjects, Scale } from 'phaser';
+import { Scene, Cameras, Display, GameObjects, Physics } from 'phaser';
 import { PhaserHelpers } from '../helpers';
 import { vector2 } from '../types';
 
@@ -11,6 +11,13 @@ export class Game extends Scene {
     pinsCategory: number;
     coinsCategory: number;
     bucketCategory: number;
+    coins: Set<Phaser.Physics.Matter.Image> = new Set();
+    physicsShape: any;
+    lastCoinId: null;
+    scoreText: GameObjects.Text;
+    coinsCountText: GameObjects.Text;
+    score: number = 0;
+    totalCoins: number = 100;
 
     constructor() {
         super('Game');
@@ -24,20 +31,43 @@ export class Game extends Scene {
     create() {
         this.camera = this.cameras.main;
         this.worldZone = this.add.zone(this.camera.centerX, this.camera.centerY, this.gamewidth, this.gameHeight);
-
+        this.coins = new Set();
         this.pinsCategory = this.matter.world.nextCategory();
         this.coinsCategory = this.matter.world.nextCategory();
         this.bucketCategory = this.matter.world.nextCategory();
+        this.physicsShape = this.cache.json.get("physicsShape");
 
         this.createBackground();
         this.createPins();
         this.handleInput();
+        this.createRewardBuckets();
+
+        // Register collision event between coin and bucket
+        this.handleCoinVsBucketCollision();
+    }
+
+    update() {
+        // Remove coins that are out of bounds
+        this.coins.forEach((coin) => {
+            if (coin.y > this.camera.height + 200) {
+                coin.destroy();
+                this.coins.delete(coin);
+            }
+        });
     }
 
     private createBackground() {
         this.background = this.add.image(this.camera.centerX, this.camera.centerY, `background-${Phaser.Math.Between(1, 2)}`).setOrigin(0.5).setDepth(0);
         this.background.setDisplaySize(this.camera.width, this.camera.height);
         Display.Align.In.Center(this.background, this.worldZone);
+
+        const coinBg = this.add.image(0, 0, "coin-bg").setDepth(1);
+        Display.Align.In.TopRight(coinBg, this.worldZone, -10, -10);
+
+        this.scoreText = PhaserHelpers.addSimpleText(`${this.score}$`, 0, 0, this);
+        Display.Align.In.Center(this.scoreText, coinBg, 20);
+
+        this.coinsCountText = PhaserHelpers.addSimpleText(`Coins Left : ${this.totalCoins}`, 200, 60, this);
     }
 
     private createPins() {
@@ -118,13 +148,59 @@ export class Game extends Scene {
         coin.setBounce(1);
         coin.setCollisionCategory(this.coinsCategory);
         coin.setCollidesWith([this.coinsCategory, this.pinsCategory, this.bucketCategory]);
-        // this.coins.add(coin);
+        this.coins.add(coin);
 
-        // update ui
-        // this.totalCoins -= 1;
-        // this.updateUI();
-
-        // // Register collision event between coin and bucket
-        // this.handleCoinVsBucketCollision();
+        // update HUD
+        this.totalCoins -= 1;
+        this.updateHUD();
     }
+
+    private handleCoinVsBucketCollision() {
+        this.matter.world.on("collisionstart", (event: Physics.Matter.Events.CollisionStartEvent) => {
+            const bodyA = event.pairs[0]?.bodyA.gameObject; // bucket
+            const bodyB = event.pairs[0]?.bodyB.gameObject; // coin
+
+            if (this.lastCoinId === bodyB.body.id) return; // prevent multiple collision
+
+            if (bodyA.body.label.startsWith("bucket") && bodyB.body.label.startsWith("coin")) {
+                bodyB.setBounce(0, 0);
+                bodyB.setMass(10000);
+                bodyB.setAngularSpeed(0);
+                bodyB.setAngularVelocity(0);
+                bodyB.setFrictionAir(0.01);
+                bodyB.setVelocity(0);
+                bodyB.setSensor(true);
+                this.lastCoinId = bodyB.body.id; // assign last coin id
+                bodyB.body.collisionFilter.mask = 0;
+
+                this.score += parseInt(bodyA.body.label.match(/\d+/)[0]) * 10;
+                this.updateHUD();
+            }
+        });
+    }
+
+    private createRewardBuckets() {
+        for (let i = 0; i < 8; i++) {
+            const rnd = Phaser.Math.Between(1, 5);
+            const key = i % 2 === 0 ? "lower-part-1" : "lower-part-2";
+            const bucket = this.matter.add.image(0, 0, key, null, {
+                shape: this.physicsShape.bucketShape,
+                label: `bucket_${rnd}`,
+                isSensor: true,
+                isStatic: true,
+            });
+            bucket.setOrigin(0.5).setScale(1.25);
+            bucket.body.position.y -= 80;
+            bucket.setCollisionCategory(this.bucketCategory);
+            Display.Align.In.BottomLeft(bucket, this.worldZone, i * -120 - 80, -40);
+            const prizeText = PhaserHelpers.addSimpleText(`x${rnd}`, bucket.x, bucket.y + 70, this);
+            prizeText.setDepth(10);
+        }
+    }
+
+    updateHUD() {
+        this.coinsCountText.setText(`Coins Left: ${this.totalCoins}`);
+        this.scoreText.setText(`${this.score}$`);
+    }
+
 }
